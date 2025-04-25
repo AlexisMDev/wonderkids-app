c3b08a573b3dc4b19ba150b4e189e035075fcfb3
Alexis Martin <alexis.martin45@orange.fr>
2025-04-25 16:16:55 +0200

    [auth] feat - Implémentation des routes d'inscription et de connexion avec JWT

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
-import playerRoutes from "./routes/players.js";
+import authRoutes from "./routes/authRoutes.js";
+import playerRoutes from "./routes/playersRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoutes);
 app.use("/api", playerRoutes);
 app.get("/", (req, res) => {
-  res.send("Wonderkids API is up and running ⚽🚀");
+	res.send("Wonderkids API is up and running ⚽🚀");
 });
 
 export default app;
--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,68 +1,80 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 const players = [
-  {
-    name: "Lamine Yamal",
-    age: 16,
-    nationality: "Spain",
-    club: "FC Barcelona",
-    position: "Winger",
-    overall: 75,
-    potential: 91,
-  },
-  {
-    name: "Arda Güler",
-    age: 18,
-    nationality: "Turkey",
-    club: "Real Madrid",
-    position: "Attacking Midfielder",
-    overall: 73,
-    potential: 89,
-  },
-  {
-    name: "Warren Zaïre-Emery",
-    age: 17,
-    nationality: "France",
-    club: "Paris Saint-Germain",
-    position: "Central Midfielder",
-    overall: 76,
-    potential: 90,
-  },
-  {
-    name: "Endrick",
-    age: 17,
-    nationality: "Brazil",
-    club: "Palmeiras / Real Madrid (futur)",
-    position: "Striker",
-    overall: 72,
-    potential: 88,
-  },
-  {
-    name: "Rico Lewis",
-    age: 19,
-    nationality: "England",
-    club: "Manchester City",
-    position: "Right Back",
-    overall: 75,
-    potential: 86,
-  },
+	{
+		name: "Lamine Yamal",
+		age: 16,
+		nationality: "Spain",
+		club: "FC Barcelona",
+		position: "Winger",
+		overall: 75,
+		potential: 91,
+	},
+	{
+		name: "Arda Güler",
+		age: 18,
+		nationality: "Turkey",
+		club: "Real Madrid",
+		position: "Attacking Midfielder",
+		overall: 73,
+		potential: 89,
+	},
+	{
+		name: "Warren Zaïre-Emery",
+		age: 17,
+		nationality: "France",
+		club: "Paris Saint-Germain",
+		position: "Central Midfielder",
+		overall: 76,
+		potential: 90,
+	},
+	{
+		name: "Endrick",
+		age: 17,
+		nationality: "Brazil",
+		club: "Palmeiras / Real Madrid (futur)",
+		position: "Striker",
+		overall: 72,
+		potential: 88,
+	},
+	{
+		name: "Rico Lewis",
+		age: 19,
+		nationality: "England",
+		club: "Manchester City",
+		position: "Right Back",
+		overall: 75,
+		potential: 86,
+	},
 ];
 
 async function main() {
-  console.log("🌱 Seeding database...");
-  for (const player of players) {
-    await prisma.player.create({ data: player });
-  }
-  console.log("✅ Done seeding.");
+	console.log("🌱 Seeding database...");
+	// Ajout des joueurs en BDD
+	for (const player of players) {
+		await prisma.player.create({ data: player });
+	}
+
+	// Ajout d'un utilisateur test en BDD
+	const hashedPassword = await bcrypt.hash("password123", 10);
+	await prisma.user.create({
+		data: {
+			email: "test@example.com",
+			password: hashedPassword,
+		},
+	});
+
+	console.log("✅ Done seeding.");
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
