243cda7a0ddfd141f36586446e43dba95fafc6d1
Alexis Martin <alexis.martin45@orange.fr>
2025-04-29 17:01:23 +0200

    [auth] feat - gestion du token et mise en place de AuthContext côté frontend

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export function AuthProvider({children}){
+  const [user, setUser] = useState(null);
+
+  useEffect( () => {
+    const token = localStorage.getItem('token');
+    if(token){
+      setUser({token});
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{user, setUser}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
